57256b6132a448994a12c8c98fc7c534aaba2b84
MuhammadIrwanaAufrizqi <161561959+MuhammadIrwanaAufrizqi@users.noreply.github.com>
2026-02-26 19:55:01 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
+// Scroll ke Contact
 function scrollToContact() {
     document.getElementById("contact").scrollIntoView({
         behavior: "smooth"
     });
+}
+
+// Animasi Scroll
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        if(entry.isIntersecting){
+            entry.target.classList.add("show");
+        }
+    });
+});
+
+document.querySelectorAll(".hidden").forEach((el) => {
+    observer.observe(el);
+});
+
+// Dark Light Mode
+function toggleMode() {
+    document.body.classList.toggle("light");
+    document.body.classList.toggle("dark");
 }
